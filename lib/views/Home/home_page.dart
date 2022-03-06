5155cb606507b297846be7ada0bc7959bf0c396e
Sunil9162 <sunilkumar8521266951@gmail.com>
2022-03-06 16:42:35 +0530

    guj

--- lib/views/Home/home_page.dart
+++ lib/views/Home/home_page.dart
@@ -88,94 +88,97 @@ class _HomePageState extends State<HomePage> {
                 padding: const EdgeInsets.only(top: 10),
                 child: Consumer<ParkingProvider>(
                   builder: (BuildContext context, _controller, Widget? child) =>
-                      SmartRefresher(
-                    header: const WaterDropHeader(),
-                    controller: _refreshController,
-                    onRefresh: () {
-                      _controller.getParkings(context).then((value) {
-                        _refreshController.refreshCompleted();
-                      });
-                    },
-                    physics: const BouncingScrollPhysics(),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              CustomIconButton(
-                                image: "assets/images/car.png",
-                                onPressed: () {
-                                  Navigator.pushNamed(context, '/forvehicle');
-                                },
-                                title: "Car",
-                              ),
-                              CustomIconButton(
-                                image: "assets/images/bus.png",
-                                onPressed: () {
-                                  Navigator.pushNamed(context, '/forvehicle');
-                                },
-                                title: "Bus",
-                              ),
-                              CustomIconButton(
-                                image: "assets/images/bike.png",
-                                onPressed: () {
-                                  Navigator.pushNamed(context, '/forvehicle');
-                                },
-                                title: "Bike",
-                              ),
-                              CustomIconButton(
-                                image: "assets/images/van.png",
-                                onPressed: () {
-                                  Navigator.pushNamed(context, '/forvehicle');
-                                },
-                                title: "Van",
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          const Text(
-                            " Nearby Parkings",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
+                      Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: SmartRefresher(
+                      header: const WaterDropHeader(),
+                      controller: _refreshController,
+                      onRefresh: () {
+                        _controller.getParkings(context).then((value) {
+                          _refreshController.refreshCompleted();
+                        });
+                      },
+                      physics: const BouncingScrollPhysics(),
+                      scrollDirection: Axis.vertical,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                CustomIconButton(
+                                  image: "assets/images/car.png",
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/forvehicle');
+                                  },
+                                  title: "Car",
+                                ),
+                                CustomIconButton(
+                                  image: "assets/images/bus.png",
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/forvehicle');
+                                  },
+                                  title: "Bus",
+                                ),
+                                CustomIconButton(
+                                  image: "assets/images/bike.png",
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/forvehicle');
+                                  },
+                                  title: "Bike",
+                                ),
+                                CustomIconButton(
+                                  image: "assets/images/van.png",
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/forvehicle');
+                                  },
+                                  title: "Van",
+                                ),
+                              ],
                             ),
-                          ),
-                          _controller.isLoading
-                              ? const Center(
-                                  child: CircularProgressIndicator(),
-                                )
-                              : _controller.parkings.isEmpty
-                                  ? SizedBox(
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.4,
-                                      child: const Center(
-                                        child: Text(
-                                          "No Parkings available",
-                                          style: TextStyle(
-                                            color: Colors.grey,
+                            const SizedBox(height: 20),
+                            const Text(
+                              " Nearby Parkings",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            _controller.isLoading
+                                ? const Center(
+                                    child: CircularProgressIndicator(),
+                                  )
+                                : _controller.parkings.isEmpty
+                                    ? SizedBox(
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.4,
+                                        child: const Center(
+                                          child: Text(
+                                            "No Parkings available",
+                                            style: TextStyle(
+                                              color: Colors.grey,
+                                            ),
                                           ),
                                         ),
+                                      )
+                                    : ListView.builder(
+                                        shrinkWrap: true,
+                                        padding: const EdgeInsets.only(top: 10),
+                                        physics:
+                                            const NeverScrollableScrollPhysics(),
+                                        itemCount: _controller.parkings.length,
+                                        itemBuilder: (context, index) {
+                                          return ParkingSlotWidget(
+                                            models: _controller.parkings[index],
+                                          );
+                                        },
                                       ),
-                                    )
-                                  : ListView.builder(
-                                      shrinkWrap: true,
-                                      padding: const EdgeInsets.only(top: 10),
-                                      physics:
-                                          const NeverScrollableScrollPhysics(),
-                                      itemCount: _controller.parkings.length,
-                                      itemBuilder: (context, index) {
-                                        return ParkingSlotWidget(
-                                          models: _controller.parkings[index],
-                                        );
-                                      },
-                                    ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
